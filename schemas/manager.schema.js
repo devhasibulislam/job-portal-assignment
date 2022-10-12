9aac37d199be006567c1cc0a87e0a353f669b821
hasibulislam999 <hasib143sl@gmail.com>
2022-10-12 08:37:52 +0600

    manager schema initialized

---
+++ schemas/manager.schema.js
@@ -0,0 +1,6 @@
+/**
+ * Title: Create an Hiring Manager model schema
+ * Description: Schema that directly validate Hiring Manager credentials and potentials
+ * Author: Hasibul Islam
+ * Date: 12/10/2022
+ */
