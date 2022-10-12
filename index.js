44999d107c9ad2c034f0a242f17b313f38eb0879
hasibulislam999 <hasib143sl@gmail.com>
2022-10-12 07:33:03 +0600

    driver code initialized

---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * Title: Job Portal Assignment
+ * Description: Create a job portal with respect the following link: https://docs.google.com/document/d/1203fnsSyBuOhnFiqhGxLWRXTy6u3nRdMzOaVAr48QA0/edit
+ * Author: Hasibul Islam
+ * Date: 12/10/2022
+ */
