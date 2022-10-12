738811b1ed7c9226ddfb727275eb6b23c2f7add3
hasibulislam999 <hasib143sl@gmail.com>
2022-10-12 13:11:12 +0600

    job route modified

--- routes/job.route.js
+++ routes/job.route.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 /* internal import */
 const jobController = require("../controllers/job.controller");
+const upload = require("../middleware/upload.middleware");
 
 /* router level connection */
 const router = express.Router();
@@ -16,5 +17,11 @@ router
   .route("/:id")
   .get(jobController.displaySpecificJob)
   .patch(jobController.modifyExistingJobCredentials);
+
+router.post(
+  "/:id/apply",
+  jobController.applyOnSpecificJobPost,
+  upload.single("resume")
+);
 
 module.exports = router;
