0fc3c514fb348f6443cb415a728e4882dff514c9
hasibulislam999 <hasib143sl@gmail.com>
2022-10-12 08:37:40 +0600

    candidate schema initialized

---
+++ schemas/candidate.schema.js
@@ -0,0 +1,6 @@
+/**
+ * Title: Create an Candidate model schema
+ * Description: Schema that directly validate Candidate credentials and potentials
+ * Author: Hasibul Islam
+ * Date: 12/10/2022
+ */
